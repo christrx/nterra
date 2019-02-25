d42c36ba432a4e95ea0098cecfa269b07fb3e7bc
Chris <christopher.trux@nterra.com>
2019-02-25 13:14:54 +0100

    css cursor change

--- public/app.js
+++ public/app.js
@@ -388,7 +388,7 @@ function saveMitarbeiter(wemail, wname) {
         Mail: wemail,
         Name: wname,
         ErfolgreichePruefungDatum: new Date(Date.UTC(2000, 00, 01)),
-        LetzterUpload: null,
+        LetzterUpload: "",
         Kennzeichen: ""
     })
 }
